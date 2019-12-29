461d6164bcf425610e3238c6200e9b91c6581aa5
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2019-12-28 20:19:20 -0500

    Add files via upload

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by User on 1/12/19.
+//  Copyright © 2019 Sey Cileli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
